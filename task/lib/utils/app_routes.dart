acfb10d9760d943986caba16ce3eb05455c63082
Abanoub romany <rabanwb04@gmail.com>
2025-10-31 15:24:43 +0200

    main initialization

---
+++ task/lib/utils/app_routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String home = '/home';
+
+
+  static final Map<String, WidgetBuilder> routes = {
+    
+  };
+}
